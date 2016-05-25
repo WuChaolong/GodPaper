36367b6ade89ead40a5b02d1d8c93eaaa4585c5f
Taifu <752761705@qq.com>
2016-05-25 16:24:40 +0800

    extend

--- extend/inIfame.js
+++ extend/inIfame.js
@@ -1,7 +1,7 @@
 
-
-var apper = createApper();
-
+if(!apper){
+	var apper = createApper();
+}
 
 function createApper(){
 	var htmlElement = document.getElementsByTagName("html")[0];
@@ -9,11 +9,10 @@ function createApper(){
 	htmlElement.style["position"]="relative";
     htmlElement.style["min-height"]="100%";
 
-	var apperHTML = '<iframe src="/apper/?key='+document.location.href
+	var apperHTML = '<iframe src="http://localhost:8080/apper/?key='+document.location.href
 		+'" style="position: absolute;width: 100%;height: 100%;top: 0;left: 0;z-index: -1;border:0;pointer-events: none;"></iframe>';
 	var apper = htmlToElement(apperHTML);
-	var cssEle = htmlToElement('<style>@-webkit-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-moz-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-o-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  } .show-prompt{position: fixed;bottom: .5em;right: -21em;z-index: 2;box-shadow: 1px 1px 5px #868686;padding: .5em;padding-left: 3.5em;transition: right 2s;background-image: url(http://localhost:8080/apper/index_files/img/1463885391767/apper.png);background-size: auto 100%;background-repeat: no-repeat;-webkit-animation: 5s ease-in-out move_eye;-moz-animation: 5s ease-in-out move_eye;-o-animation: 5s ease-in-out move_eye;animation: 5s ease-in-out move_eye;}.show-prompt:hover{right:0;animation-play-state: paused;}</style>');
-	
+	var cssEle = htmlToElement('<style>@-webkit-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-moz-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-o-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  } .show-prompt{position: fixed;bottom: .5em;right: -21em;z-index: 2;box-shadow: 1px 1px 5px #868686;padding: .5em;padding-left: 3.5em;transition: right 2s;background-image: url(http://localhost:8080/apper/index_files/img/1463885391767/apper.png);background-size: auto 100%;background-repeat: no-repeat;-webkit-animation: 5s ease-in-out move_eye;-moz-animation: 5s ease-in-out move_eye;-o-animation: 5s ease-in-out move_eye;animation: 5s ease-in-out move_eye;font-family: initial;}.show-prompt:hover{right:0;animation-play-state: paused;}</style>');
 	document.body.appendChild(apper);
 	document.body.appendChild(cssEle);
 	addDblclik(htmlElement,apper.contentWindow);
