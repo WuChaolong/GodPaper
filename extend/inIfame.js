783b4d3453f99af494062bbc8d95dd2cd53797f9
Taifu <752761705@qq.com>
2016-06-03 15:02:06 +0800

    getSelection

--- extend/inIfame.js
+++ extend/inIfame.js
@@ -13,7 +13,7 @@ function createPaaper(){
 	var paaperHTML = '<iframe src="'+protocol+'//wuchaolong.github.io/paaper/?key='+location.href
 		+'" style="position: absolute;width: 100%;height: 100%;top: 0;left: 0;z-index: -1;border:0;pointer-events: none; max-height: 2000px;max-width: 2000px;box-shadow: rgb(187, 191, 194) 1px 1px 5px;"></iframe>';
 	var paaper = htmlToElement(paaperHTML);
-	var cssEle = htmlToElement('<style>@-webkit-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-moz-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-o-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  } .show-prompt{position: fixed;bottom: .5em;right: -21em;z-index: 2;box-shadow: 1px 1px 5px #868686;padding: .5em;padding-left: 3.5em;transition: right 2s;background-image: url(//wuchaolong.github.io/paaper/index_files/img/1463885391767/paaper.png);background-size:3.5em 100%;background-repeat: no-repeat;-webkit-animation: 5s ease-in-out move_eye;-moz-animation: 5s ease-in-out move_eye;-o-animation: 5s ease-in-out move_eye;animation: 5s ease-in-out move_eye;font-family: initial;}.show-prompt:hover{right:0;animation-play-state: paused;}</style>');
+	var cssEle = htmlToElement('<style>@-webkit-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-moz-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-o-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  } .show-prompt{position: fixed;bottom: .5em;right: -21em;z-index: 2;box-shadow: 1px 1px 5px #868686;padding: .5em;padding-left: 3.5em;transition: right 2s;background-image: url(//wuchaolong.github.io/paaper/extend/1464654409058/paaper.png);background-size:3.5em 100%;background-repeat: no-repeat;-webkit-animation: 5s ease-in-out move_eye;-moz-animation: 5s ease-in-out move_eye;-o-animation: 5s ease-in-out move_eye;animation: 5s ease-in-out move_eye;font-family: initial;}.show-prompt:hover{right:0;animation-play-state: paused;}</style>');
 	document.body.appendChild(paaper);
 	document.body.appendChild(cssEle);
 	try{
@@ -25,8 +25,10 @@ function createPaaper(){
 }
 function addDblclik(e1,e2){
 	e2.ondblclick = null;
-	e1.ondblclick = function(){
-
+	e1.ondblclick = function(e){
+		if(!(window.getSelection().toString() == false||e.target.className=="show-prompt")){
+			return;
+		}
 		var htmlElement = document.getElementsByTagName("html")[0];
 		if(paaper.style["pointer-events"] == "none"){
 			openWritePaaper();
