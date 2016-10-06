c336a05a1f5fb7be9141d2cb58ecc06c83ad4dfb
Taifu <752761705@qq.com>
2016-10-06 14:55:35 +0800

    local

--- extend/inIfame.js
+++ extend/inIfame.js
@@ -10,16 +10,16 @@ function createPaaper(){
 	htmlElement.style["position"]="relative";
     htmlElement.style["min-height"]="100%";
 	var protocol = location.protocol=="https:"?"https:":"http:";
+	var paaperDiv = htmlToElement("<div id='paaperDiv'></div>");
+	document.body.appendChild(paaperDiv);
 	var paaperHTML = '<iframe src="'+protocol+'//wuchaolong.github.io/paaper/?key='+location.href
 		+'" style="position: absolute;width: 100%;height: 0;top: 0;left: 0;z-index: -1;border:0;pointer-events: none; max-height: 2000px;max-width: 2000px;box-shadow: rgb(187, 191, 194) 1px 1px 5px;"></iframe>';
 	var paaper = htmlToElement(paaperHTML);
 	var cssEle = htmlToElement('<style>@-webkit-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-moz-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@-o-keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  }@keyframes move_eye { from { right:0;}  60%{right:0;} to  { right:-21em;}  } .show-prompt{position: fixed;bottom: .5em;right: -18em;z-index: 2;box-shadow: 1px 1px 5px #868686;padding: .5em;padding-left: 3.5em;transition: right 2s;background-image: url(//wuchaolong.github.io/paaper/extend/1464654409058/paaper.png);background-size:3.5em 100%;background-repeat: no-repeat;-webkit-animation: 5s ease-in-out move_eye;-moz-animation: 5s ease-in-out move_eye;-o-animation: 5s ease-in-out move_eye;animation: 5s ease-in-out move_eye;font-family: initial;}.show-prompt:hover{right:0;animation-play-state: paused;}</style>');
-	document.body.appendChild(paaper);
+	paaperDiv.appendChild(paaper);
 	iframeOnload(paaper,function(){
 		paaper.style.height = "100%";
-		openWritePaaper();
-		document.body.appendChild(cssEle);
-		
+		document.getElementById("paaperDiv").appendChild(cssEle);
 	});
 	try{
 		addDblclik(htmlElement,paaper.contentWindow);
@@ -70,7 +70,7 @@ function onlyReadPaaper(){
 
 function showPrompt(html){
 	var element = htmlToElement(html);
-	document.body.appendChild(element); 
+	document.getElementById("paaperDiv").appendChild(element); 
 }
 
 
